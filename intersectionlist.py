87737d8bf39de704f03b793abe2bfb26d33b841f
Rakshitha94 <rakshithalr3007@gmail.com>
2019-11-01 20:18:22 -0700

    Please Review

--- intersectionlist.py
+++ intersectionlist.py
@@ -1,14 +1,11 @@
-# leetcode: failute
-# timecomplexity: O(m+n)
-# no ddoubts
-# code not executed
-
-# expliantion:
-# 1. take the lenghts of the two lists
-# 2. find the differnece between the two lenghts as diff
-# 3. traverse the longer list n times
-# 4. checks the values of the node
-# 5. If the values are same, return it
+# leetcode:accepted
+# time complexity: o(N)
+# space complexity: O(1)
+# no doubts
+# explination:
+# you need to find the enght of the two linkedlist
+# make sure the two linkedlist are pointing to the point;
+# iterate through both the linkedlist until youn find intersection
 
 
 # Definition for singly-linked list.
@@ -23,37 +20,33 @@ class Solution(object):
         :type head1, head1: ListNode
         :rtype: ListNode
         """
-        p1 = headA
-        p2 = headB
         lenA = 0
         lenB = 0
-        # finding length of A
-        while (p1 != None):
-            p1 = p1.next
-            lenA += 1
-        # finding length of B
-        while (p2 != None):
-            p2 = p2.next
-            lenB += 1
-        # finding the diffrence betwee the two lenghts
-        diff = abs(lenA - lenB)
-        print(lenA, lenB)
-        # print(p2.val)
+        currA = headA
+        currB = headB
 
-        # traversing the longest list "diff" times
-        if lenA > lenB:
-            while (diff > 0):
-                p1 = p1.next
-                print("first is longer")
-                diff = diff - 1
-        if lenB > lenA:
-            print("move {} times".format(diff))
-            while (diff > 0):
-                print("second is longer")
-                p2 = p2.next
-                diff = diff - 1
-                # checking if p1.val same as p2.val
-        while (p1.val == p2.val):
-            p1 = p1.next
-            p2 = p2.next
-        return p1.val
+        # finding the lenght of the linkedlist A
+        while (currA != None):
+            # print(currA)
+            currA = currA.next
+            lenA += 1
+
+        # finding the length of the linkedlist B
+        while (currB != None):
+            currB = currB.next
+            lenB += 1
+        # if lnkedlist A is longer than B, move head A such that len(head A --- end )is equal to the len  of B
+        while (lenA > lenB):
+            headA = headA.next
+            lenA -= 1
+        # repeat the same if linkedlist B is longer than A
+        while (lenB > lenA):
+            headB = headB.next
+            lenB -= 1
+
+        # keep on shifting until the values of the two pointer becomes the same
+        while (headA != headB):
+            headA = headA.next
+            headB = headB.next
+        return headA
+
