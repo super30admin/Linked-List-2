bd8602aee57706fd063a354835124928a8136aa5
sindhu819 <sindu819@gmail.com>
2019-11-01 20:37:06 -0700

    Add files via upload

--- Problem-53.py
+++ Problem-53.py
@@ -56,4 +56,44 @@ class Solution(object):
             else:
                 headB=headB.next
         return None
+    
+# optimal solution - Time complexity -O(N), space -O(1)
+# Approach - We will traverse both headA and headB and we calculate the length of both A and B. If length of headA is greater than length of headB, then we increment the headA till the difference and we going forward were both the pointers meet.
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+       
+        if headA==None or headB == None:
+            return None
+        len_A=0
+        len_B=0
+        cur_A=headA
+        cur_B=headB
+        while cur_A!=None:
+            cur_A=cur_A.next
+            len_A+=1
+        
+        while cur_B!=None:
+            cur_B=cur_B.next
+            len_B=len_B+1
+        
+        for i in range(abs(len_A-len_B)):
+            if len_A>len_B:
+                headA=headA.next
+            else:
+                headB=headB.next
+        while headA!=headB:
+            headA=headA.next
+            headB=headB.next
+        return headA
 
