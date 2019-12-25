95bb2b95deda3779289d70bd3cf279058cada501
Akhil Raveendran <itsrakhil163@gmail.com>
2019-12-25 14:29:19 -0800

    Update ReorderList.java

--- ReorderList.java
+++ ReorderList.java
@@ -1,18 +1,8 @@
 Time Complexity-O(n)
 Space Complexity-O(1)
 
-class Solution { 
+class Solution {
     
-    public void merge(ListNode p1,ListNode p2,ListNode middle)
-    {
-            while(p1!=middle){
-                middle.next=p2.next;
-                p2.next=p1.next;
-                p1.next=p2;
-                p1=p2.next;
-                p2=middle.next;
-            }
-    }
     private ListNode reverse(ListNode head)
     {
         ListNode curr=head;
@@ -39,8 +29,22 @@ class Solution {
         }
         
         ListNode middle=slow;
-        slow.next=reverse(slow.next);
-        merge(head,middle.next,middle);
+        fast=reverse(slow.next);
+        
+        slow.next=null;
+        slow=head;
+        while(slow!=null && fast!=null)
+        {
+            ListNode temp=fast.next;
+            fast.next=slow.next;
+            slow.next=fast;
+            slow=fast.next;
+            fast=temp;
+            
+            
+        }
+        
+     
         return;
         
     }
