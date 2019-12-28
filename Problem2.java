5ef4a2d9e5baeb9744e986b555170e3cffeeed45
Sweety Sojrani <sweety.sojrani@sjsu.edu>
2019-12-27 18:04:28 -0800

    Update Problem2.java

--- Problem2.java
+++ Problem2.java
@@ -1,6 +1,6 @@
 // Time Complexity - O(1) we are not traversing to the entire list. We are given reference to the node that needs to be delected.
 // Space Complexity - O(1) no extra space is used
-// We need to swap the data of the node that needs to be deleted with the next node data. And then delete the next node. 
+// We need to swap the data of the node that needs to be deleted with the next node data and then delete the next node. 
 
 void deleteNode(Node node)
     {
