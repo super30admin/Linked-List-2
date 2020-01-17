fda2d6f7a74032e50a7253514c7d88fc20c2b409
Bhushan Vasisht <bhushanvasisht@Bhushans-MacBook-Air.local>
2020-01-17 17:25:21 -0600

    O(m+n)

---
+++ Problem3.java
@@ -0,0 +1,35 @@
+// Time Complexity :
+//      m is the number of nodes in headA
+//		n is the number of nodes in headB
+//      getIntersectionNode() - O(m + n)
+//      
+// Space Complexity :
+//      getIntersectionNode() - O(n)
+//
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+public class Solution {
+    HashSet<ListNode> set = new HashSet<>();
+    
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA == null || headB == null)
+            return null;
+        
+        while(headA != null)
+        {
+            set.add(headA);
+            headA = headA.next;
+        }
+        
+        while(headB != null)
+        {
+            if(set.contains(headB))
+                return headB;
+            
+            headB = headB.next;
+        }
+        
+        return null;
+    }
+}
