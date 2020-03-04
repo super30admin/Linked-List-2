0dfe6f5da6df4475524e01add211169e17937ef1
priyanka1012-singhal <priyanka.singhal@sjsu.edu>
2020-03-03 17:34:47 -0800

    BST iterator - Please review

--- Sample.java
+++ Sample.java
@@ -5,3 +5,42 @@
 
 
 // Your code here along with comments explaining your approach
+
+// Problem1 (https://leetcode.com/problems/binary-search-tree-iterator/)
+//Average case Time complexity O(1) Space Complexity O(height)
+//Worst case Time Complexity O(n)
+class BSTIterator {
+
+    Stack<TreeNode> stack;
+    public BSTIterator(TreeNode root) {
+        stack = new Stack<>();
+        TreeNode node = root;
+        dfs(node);
+    }
+    
+    /** @return the next smallest number */
+    public int next() {
+        TreeNode curr = stack.pop();
+        dfs(curr.right);
+        return curr.val;
+    }
+    
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+    
+    private void dfs(TreeNode node){
+        while(node != null){
+            stack.push(node);
+            node = node.left;
+        }
+    }
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
