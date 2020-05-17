7a2eb1d573b21e73677158c61fbf46b90914d780
Namrata <namratakasar25@gmail.com>
2020-05-16 19:03:35 -0700

    Completed first problem

--- Sample.java
+++ Sample.java
@@ -5,3 +5,35 @@
 
 
 // Your code here along with comments explaining your approach
+class Sample {
+
+    class Node
+    {
+        int data ;
+        Node next;
+        Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+
+    // Time Complexity : O(1)
+    // Space Complexity : O(1)
+    // Did this code successfully run on GFG : Yes
+    // Any problem you faced while coding this : No
+
+
+    // Problem 3
+    /**
+     * 1. Since the node is not last element in the list, we can copy the next node's data into this node.
+     * 2. Then point its next as next node's next.
+     */
+    void deleteNode(Node node)
+    {
+         // Your code here
+         node.data = node.next.data;
+         node.next = node.next.next;
+         return;
+    }
+}
