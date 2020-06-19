631b6c3e4b4e79f46ef340e7aa7e0abe537a0380
chirag <cr.tagadiya@gmail.com>
2020-06-19 19:08:07 -0400

    interrsection of linkedlist

--- IntersectionLinkedList.java
+++ IntersectionLinkedList.java
@@ -12,6 +12,53 @@
 */
 
 public class IntersectionLinkedList {
+
+
+/*
+    Maintain two pointers pApA and pBpB initialized at the head of A and B, respectively. Then let them both traverse through the lists, one node at a time.
+    When pApA reaches the end of a list, then redirect it to the head of B (yes, B, that's right.); similarly when pBpB reaches the end of a list, redirect it the head of A.
+    If at any point pApA meets pBpB, then pApA/pBpB is the intersection node.
+    To see why the above trick would work, consider the following two lists: A = {1,3,5,7,9,11} and B = {2,4,9,11}, which are intersected at node '9'. Since B.length (=4) < A.length (=6), pBpB would reach the end of the merged list first, because pBpB traverses exactly 2 nodes less than pApA does. By redirecting pBpB to head A, and pApA to head B, we now ask pBpB to travel exactly 2 more nodes than pApA would. So in the second iteration, they are guaranteed to reach the intersection node at the same time.
+    If two lists have intersection, then their last nodes must be the same one. So when pApA/pBpB reaches the end of a list, record the last element of A/B respectively. If the two last elements are not the same one, then the two lists have no intersections.
+
+*/
+    public ListNode getIntersectionNode_TwoPass_Solution(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null ) return null;   
+        ListNode temp1 = headA;
+        ListNode temp2 = headB;
+        
+        int swapA = 0;
+        int swapB = 0;
+        
+        while(true){
+            // if found temp1 eauals temp2 return this is intersection point
+            if (temp1 ==  temp2 ){
+                return temp2;
+            }
+            //  if one of the list ends points it to other list head
+            if(temp1 != null){
+                temp1 =  temp1.next;
+            }else{
+                temp1 = headB;
+                swapA++;
+            }
+        
+            if(temp2 != null){
+                temp2 =  temp2.next;
+            }else{
+                temp2 = headA;
+                swapB++;
+            }
+            // traverse the list till you reach end 
+            // if no intersection then in two pass you won't find any 
+            // return null
+            if (swapA  == 2 && swapB == 2 ){
+                return null;
+            }
+            
+        }
+    }
+
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         HashSet<ListNode>  set  =  new HashSet<ListNode>();
         
