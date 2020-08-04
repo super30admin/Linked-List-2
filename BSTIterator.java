bbe7f3559a8c5393977dccf3fd833c870eea3bfb
Santosh Kallala <kallalasantosh@gmail.com>
2020-08-03 20:04:53 -0400

    Intersection Of LL

---
+++ BSTIterator.java
@@ -0,0 +1,15 @@
+class BSTIterator{
+    public BSTIterator(TreeNode root) {
+
+    }
+
+    /** @return the next smallest number */
+    public int next() {
+
+    }
+
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+
+    }
+}
---
+++ IntersectionOfLinkedList.java
@@ -0,0 +1,33 @@
+class IntersectionOfLinkedList{
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int lenA = length(headA), lenB = length(headB);
+        // move headA and headB to the same start point
+        while (lenA > lenB) {
+            headA = headA.next;
+            lenA--;
+        }
+        while (lenA < lenB) {
+            headB = headB.next;
+            lenB--;
+        }
+        // find the intersection until end
+        while (headA != headB) {
+            headA = headA.next;
+            headB = headB.next;
+        }
+        return headA;
+    }
+
+    private int length(ListNode node) {
+        int length = 0;
+        while (node != null) {
+            node = node.next;
+            length++;
+        }
+        return length;
+    }
+
+    //time complexity : o(m+n)
+    //space :0(1)
+    //approach : Get the length of the two lists. Align them to the same start point. Move them together until finding the intersection point, or the end null
+}
