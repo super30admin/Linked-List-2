cbe3a17c9cfce897c71e6eec54c6a02ae763f82b
asureban1 <54301735+asureban1@users.noreply.github.com>
2020-09-15 00:03:33 -0500

    Update BinarySearchTreeIterator.java

--- BinarySearchTreeIterator.java
+++ BinarySearchTreeIterator.java
@@ -1,5 +1,5 @@
 // Time Complexity : O(n)
-// Space Complexity : O(n)
+// Space Complexity : O(h), h = height of the tree
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this : No
 
@@ -19,30 +19,31 @@
  * }
  */
 class BSTIterator {
-    ArrayList<Integer> list = new ArrayList();
+    
+    Stack<TreeNode> stack = new Stack();
+
     public BSTIterator(TreeNode root) {
         
-        Stack<TreeNode> stack = new Stack();
-        while(root != null || !stack.isEmpty()){
-            if(root != null){
-                stack.push(root);
-                root = root.left;
-            }else{
-                TreeNode node = stack.pop();
-                list.add(node.val);
-                root = node.right;
-            }
-        }
+        dfs(root);
     }
     
     /** @return the next smallest number */
     public int next() {
-        return list.remove(0);
+        TreeNode node = stack.pop();
+        if(node.right != null)  dfs(node.right);
+        return node.val;
     }
     
     /** @return whether we have a next smallest number */
     public boolean hasNext() {
-        return !list.isEmpty();
+        return !stack.isEmpty();
+    }
+    
+    private void dfs(TreeNode node){
+        while(node != null){
+            stack.push(node);
+            node = node.left;
+        }
     }
 }
 
