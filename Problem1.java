56822de223e115672cfd9b3d4fc25aa2a4ef63a7
Hanish Talasila <t.hanishsai@gmail.com>
2020-10-11 15:29:37 -0600

    Update Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -1,7 +1,59 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : O(N)
+// Space Complexity : O(N)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
 
 
-// Your code here along with comments explaining your approach
+// Your code here along with comments explaining your approach:  Storing the left most values in the stack and then right and then poping
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BSTIterator {
+    Stack<TreeNode> stack;
+    public BSTIterator(TreeNode root) {
+        stack = new Stack<TreeNode>();
+        dfs(root);
+    }
+    
+    /** @return the next smallest number */
+    public int next() {
+        if(!hasNext()) return -1;
+        TreeNode Node = stack.pop();
+        if(Node.right != null){
+            dfs(Node.right);
+        }
+        return Node.val;
+    }
+    
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        return !stack.empty();
+    }
+    
+    public void dfs( TreeNode root) {
+        while(root != null){
+            stack.push(root);
+            root = root.left;
+        }      
+    }
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
