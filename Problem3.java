c9cf2586b7930713cd556ed794fd43becb01ab4e
Hanish Talasila <t.hanishsai@gmail.com>
2020-10-11 15:35:19 -0600

    Update Problem3.java

--- Problem3.java
+++ Problem3.java
@@ -1,7 +1,28 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : O(N)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
 
 
-// Your code here along with comments explaining your approach
+// Your code here along with comments explaining your approach: Assigning the value to the last node and changing the reference.
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+        
+       if(node.next == null)
+       {
+            return;
+        }
+        
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
