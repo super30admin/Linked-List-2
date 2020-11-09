95292d92930847fbd14fd54b9f55c2ac9eae6808
JaiVora <52439248+JaiVora@users.noreply.github.com>
2020-11-09 01:29:35 -0500

    Update BSTIterator.java

--- BSTIterator.java
+++ BSTIterator.java
@@ -1,45 +1,37 @@
 //Time Complexity: O(n)
-//Space Complexity: O(1)
+//Space Complexity: O(h) | h is the height of the tree
 //Program ran successfully
 /*
-    Algorithm: 1. The problem is solved in 3 steps: find the middle element of the list, reverse the second half of the list & merge both the lists.
-               2. Middle is found by hare and tortoise method
-               3. Reversing of the list is done by maintaining three pointers: slow,fast and previous
-               4. Two lists are merged doing pointer manipulation
+    Algorithm: 1. We perform a controlled a recursion. 
+               2. A stack is maintained and the elements to the left of the root are pushed onto the stack.
+               3. Once a next function call is made, we pop the top of the stack and check if there is a right node of the popped node.
+               4. If there is a right node, we again push elements to the left of the root onto the stack
 */
-class Solution {
-    public void reorderList(ListNode head) {
-        //edge cases
-        if(head == null || head.next == null) return;
-        ListNode slow = head;
-        ListNode fast = head;
-        while(fast.next != null && fast.next.next != null){
-            fast = fast.next.next;
-            slow = slow.next;
-        }
-        fast = reverse(slow.next);
-        slow.next = null;
-        slow = head;
-        while(fast != null){
-            ListNode temp = slow.next;
-            slow.next = fast;
-            fast = fast.next;
-            slow.next.next = temp;
-            slow = temp;
-        }
-        return;
+class BSTIterator {
+    Stack<TreeNode> st;
+    public BSTIterator(TreeNode root) {
+        st = new Stack<>();
+        dfs(root);   
     }
-    private ListNode reverse(ListNode head){
-        ListNode prev = null;
-        ListNode slow = head;
-        ListNode fast = head.next;
-        while(fast!=null){
-            slow.next = prev;
-            prev = slow;
-            slow = fast;
-            fast = fast.next;
+    
+    /** @return the next smallest number */
+    public int next() {
+        TreeNode returnNode = st.pop();
+        if(returnNode.right != null){
+            dfs(returnNode.right);
         }
-        slow.next = prev;
-        return slow;
+        return returnNode.val;
     }
+    
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        return !st.isEmpty();
+    }
+    
+    private void dfs(TreeNode root){
+        while(root != null){
+            st.push(root);
+            root = root.left;
+        }
+    } 
 }
