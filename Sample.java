9b033b397c3915abf8457f3b5737afa68d31d8f5
Shivi_Bhatt <in.shivi@gmail.com>
2020-12-21 13:46:48 -0800

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -5,3 +5,40 @@
 
 
 // Your code here along with comments explaining your approach
+
+
+public void reorderList(ListNode head) {
+    if (head == null) return;
+
+
+    ListNode slow = head, fast = head;
+    while (fast != null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+
+
+    ListNode prev = null, curr = slow, tmp;
+    while (curr != null) {
+      tmp = curr.next;
+
+      curr.next = prev;
+      prev = curr;
+      curr = tmp;
+    }
+
+  
+    ListNode first = head, second = prev;
+    while (second.next != null) {
+      tmp = first.next;
+      first.next = second;
+      first = tmp;
+
+      tmp = second.next;
+      second.next = first;
+      second = tmp;
+    }
+  }
+
+// Time complexity : O(N)
+// Space Complexty : O (1)
