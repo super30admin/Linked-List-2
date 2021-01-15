34c52ef88bad2648203c141ca231c08d30ace984
Ashish Mishra <ashishk861@gmail.com>
2021-01-15 12:21:43 -0500

    Create Problem_3.java

---
+++ Problem_3.java
@@ -0,0 +1,39 @@
+// Time Complexity :
+// Space Complexity :
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        HashMap<ListNode,ListNode> hash=new HashMap<ListNode,ListNode>();
+        ListNode temp=headA;
+        while(temp!=null){
+            hash.put(temp,temp);
+            temp=temp.next;
+        }
+        temp=headB;
+        while(temp!=null){
+            if(hash.containsKey(temp)){
+                return temp;
+            }
+            temp=temp.next;
+        }
+        return null;
+    }
+}
