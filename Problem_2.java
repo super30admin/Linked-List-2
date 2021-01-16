3943c0134a791affbd80184d7b85d508fb090641
Ashish Mishra <ashishk861@gmail.com>
2021-01-16 11:21:49 -0500

    Update Problem_2.java

--- Problem_2.java
+++ Problem_2.java
@@ -5,3 +5,65 @@
 
 
 // Your code here along with comments explaining your approach
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+    // public void reorderList(ListNode head) {
+        // int size=0;
+        // ListNode temp=head;
+        // while(temp!=null){
+            // size=size+1;
+            // temp=temp.next;
+        // }
+        // int[] arr=new int[size];
+        // size=0;
+        // temp=head;
+        // while(temp!=null){
+            // arr[size++]=temp.val;
+            // temp=temp.next;
+        // }
+        // if(arr.length==1){
+            // head=new ListNode(arr[0]);
+            // return;
+        // }
+        // if(arr.length==2){
+            // head=new ListNode(arr[0]);
+            // temp=head;
+            // temp.next=new ListNode(arr[1]);
+            // return;
+        // }
+        // int i=1;
+        // int j=arr.length-1;
+        // while()
+        // System.out.println(Arrays.toString(arr));
+        
+    // }
+// }
