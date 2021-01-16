6aee03433f3c47412d7c30f64f6165fd9850d38b
Ashish Mishra <ashishk861@gmail.com>
2021-01-16 11:21:54 -0500

    Update Problem_4.java

--- Problem_4.java
+++ Problem_4.java
@@ -5,3 +5,34 @@
 
 
 // Your code here along with comments explaining your approach
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        HashMap<ListNode,ListNode> hash=new HashMap<ListNode,ListNode>();
+        ListNode temp=headA;
+        while(temp!=null){
+            hash.put(temp,temp);
+            temp=temp.next;
+        }
+        temp=headB;
+        while(temp!=null){
+            if(hash.containsKey(temp)){
+                return temp;
+            }
+            temp=temp.next;
+        }
+        return null;
+    }
+}
