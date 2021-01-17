bc64811ede623cd9c431c89b8f2e43b31f452c97
Ashish Mishra <ashishk861@gmail.com>
2021-01-17 13:06:44 -0500

    Update Problem_4.java

--- Problem_4.java
+++ Problem_4.java
@@ -1,6 +1,6 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
+// Time Complexity :O(n)
+// Space Complexity :O(n)
+// Did this code successfully run on Leetcode :Yes
 // Any problem you faced while coding this :
 
 
@@ -34,5 +34,75 @@ public class Solution {
             temp=temp.next;
         }
         return null;
+    }
+}
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+ 
+// Time Complexity :O(n)
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int lenA=0;
+        int lenB=0;
+        ListNode temp=headA;
+        while(temp!=null){
+            lenA=lenA+1;
+            temp=temp.next;
+        }
+        temp=headB;
+        while(temp!=null){
+            lenB=lenB+1;
+            temp=temp.next;
+        }
+        //System.out.println(lenA+","+lenB);
+        int diff=0;
+        if(lenA>lenB){
+            diff=lenA-lenB;
+            temp=headA;
+            while(diff>0){
+                temp=temp.next;
+                diff=diff-1;
+            }
+            headA=temp;
+            while(headA!=headB){
+                headA=headA.next;
+                headB=headB.next;
+            }
+            return headA;
+        }else{
+            diff=lenB-lenA;
+            temp=headB;
+            while(diff>0){
+                temp=temp.next;
+                diff=diff-1;
+            }
+            headB=temp;
+            while(headA!=headB){
+                headA=headA.next;
+                headB=headB.next;
+            }
+            return headA;
+            //System.out.println(temp.val);
+        }
+        //return headA;
+        
     }
 }
