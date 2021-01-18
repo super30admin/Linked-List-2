bd975364bf523b8103c8842351c587b1885de36f
KusumaYReddy <64619968+KusumaYReddy@users.noreply.github.com>
2021-01-18 06:26:30 -0500

    Update reorder-list.java

--- reorder-list.java
+++ reorder-list.java
@@ -1,12 +1,7 @@
 /**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
+ 
+ Time complexity O(n)
+ 
  */
 class Solution {
     public void reorderList(ListNode head) {
