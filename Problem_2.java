94ddfc972603fd91b3c829fbeb59e2e411deb200
Ashish Mishra <ashishk861@gmail.com>
2021-01-17 23:25:11 -0500

    Update Problem_2.java

--- Problem_2.java
+++ Problem_2.java
@@ -1,15 +1,83 @@
-// Time Complexity :
-// Space Complexity :
+// Time Complexity :O(n)
+// Space Complexity :O(1)
 // Did this code successfully run on Leetcode :
 // Any problem you faced while coding this :
 
 
 // Your code here along with comments explaining your approach
 
+//Finding the middle then reversing the second part of the linked list.
+//At last we can merge the two created linkedlist
 
 
 
-
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        if(head==null) return;
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast.next!=null && fast.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        //System.out.println(slow.val);
+        ListNode newPointer=RevreseList(slow.next);
+        slow.next=null;
+        //printList(head);
+        //printList(newPointer);
+        ListNode temp=null;
+        slow=head;
+        while(newPointer!=null){
+            temp=slow.next;
+            slow.next=newPointer;
+            newPointer=newPointer.next;
+            slow.next.next=temp;
+            slow=temp;
+            
+        }
+        //return head;
+    }
+    
+    public ListNode RevreseList(ListNode node){
+        if(node==null){
+            return null;
+        }
+        ListNode prev=null;
+        ListNode curr=node;
+        ListNode fast=node.next;
+        while(fast!=null){
+            curr.next=prev;
+            prev=curr;
+            curr=fast;
+            fast=fast.next;
+        }
+        curr.next=prev;
+        return curr;
+    }
+    
+    public void printList(ListNode node){
+        if(node==null){
+            return;
+        }
+        ListNode temp=node;
+        while(temp!=null){
+            System.out.print(temp.val+"->");
+            temp=temp.next;
+        }
+        System.out.print("null");
+        System.out.println("");
+    }
+}
 
 
 
