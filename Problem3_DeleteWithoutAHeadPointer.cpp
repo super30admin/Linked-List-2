091bc87af21a30472cb78cd5b48ca0ce819dca00
Saphal Thammu <saphalthammu@Saphals-MacBook-Pro.local>
2021-01-20 16:28:37 -0700

    Completed Problem 3 delete without head pointer

---
+++ Problem3_DeleteWithoutAHeadPointer.cpp
@@ -0,0 +1,80 @@
+
+// Time Complexity : O(n) where n is number of elements
+// Space Complexity : O(1) we are not using extra space
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this : no
+
+
+// Your code here along with comments explaining your approach
+
+
+#include<vector>
+#include<bits/stdc++.h>
+#include<stdlib.h>
+#include<cmath>
+#include<time.h>
+#include<iostream>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
+class Solution{
+public: 
+    void deleteNode(ListNode *node){
+        node->val=node->next->val;
+        node->next=node->next->next;// Your code here
+
+    }
+   
+
+
+};
+
+
+
+
+int main(){
+
+    
+
+
+    Solution a;
+
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    // head->next->next->next->next = newNode(5);
+
+    // vector<int>  nums={0,0,1,1,1,1,2,3,3};
+    //int target=5;
+    a.deleteNode(head->next);
+    // for (int x : nums) 
+    //      cout << x << " "; 
+
+    // cout<<endl;
+
+    while(head!=NULL){
+        cout<<head->val<<" ";
+        if (head->next)
+            cout << "-> ";
+        head=head->next;
+    }
+
+    // can either do this below for answer in true false 
+    // cout.setf(std::ios::boolalpha);
+    // cout <<  b << endl;
+
+    // or
+
+    // cout <<  boolalpha << b << endl;
+
+}
