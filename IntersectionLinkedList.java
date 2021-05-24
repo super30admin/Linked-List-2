ddbd30655d903ca8bc35e66adaebc2e93b32b3b0
sp9491 <42806187+sp9491@users.noreply.github.com>
2021-05-24 07:13:46 -0400

    Create IntersectionLinkedList.java

---
+++ IntersectionLinkedList.java
@@ -0,0 +1,15 @@
+//time complexity - O(n*m)
+//space complexity - O(1)
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        while (headA != null) {
+            ListNode pB = headB;
+            while (pB != null) {
+                if (headA == pB) return headA;
+                pB = pB.next;
+            }
+            headA = headA.next;
+        }
+        return null;
+    }
+}
