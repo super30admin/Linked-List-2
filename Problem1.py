c0eac7897878a8c6123b44c630e918212c94f380
manojvarmarudraraju <46211625+manojvarmarudraraju@users.noreply.github.com>
2021-06-20 18:27:06 +0530

    Completed Problem 1,2,3,4

--- Problem1.py
+++ Problem1.py
@@ -1,4 +1,39 @@
-# Time Complexity : 
-# Space Complexity : 
-# Did this code successfully run on Leetcode : 
-# Any problem you faced while coding this : 
+# Time Complexity : O(1)
+# Space Complexity : O(h)
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : No
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+#use custom stack to stop the iteration wherever needed
+class BSTIterator:
+    
+    def __init__(self, root: TreeNode):
+        self.arr = []
+        self.__dfs(root)
+        
+    def __dfs(self,root: TreeNode):
+        node = root
+        while node != None:
+            self.arr.append(node)
+            node = node.left   
+
+    def next(self) -> int:
+        node = self.arr.pop()
+        self.__dfs(node.right)
+        return node.val
+        
+
+    def hasNext(self) -> bool:
+        return len(self.arr) != 0
+
+
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
--- Problem4.py
+++ Problem4.py
@@ -1,4 +1,42 @@
-# Time Complexity : 
-# Space Complexity : 
-# Did this code successfully run on Leetcode : 
-# Any problem you faced while coding this : 
+# Time Complexity : O(N)
+# Space Complexity : O(1)
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : No
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+#iterating through the linked list count the elements
+#check the difference and start from that to find where they meet
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        nodeA = headA
+        countA = 0
+        while nodeA != None:
+            countA += 1
+            nodeA = nodeA.next
+        nodeB = headB
+        countB = 0
+        while nodeB != None:
+            countB += 1
+            nodeB = nodeB.next
+        a,b = None,None
+        if countA > countB:
+            a,b = headA,headB
+        else:
+            a,b = headB,headA
+        diff = abs(countA-countB)
+        
+        while diff != 0:
+            diff -= 1
+            a = a.next
+        while a != b:
+            a = a.next
+            b = b.next
+        
+        return a
