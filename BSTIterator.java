3255373961b6955ddfa3dc631dad2c836adeef50
Ashwath Santhanam <assantha@visa.com>
2021-08-16 20:58:04 -0700

    LinkedList-2 Completed

---
+++ BSTIterator.java
@@ -0,0 +1,36 @@
+//https://leetcode.com/problems/binary-search-tree-iterator/
+/*
+Time: Next() is Amortized O(1), hasNext() is O(1)
+Space: O(h)
+Did this code successfully run on Leetcode : Yes
+Any problem you faced while coding this : None
+*/
+public class BSTIterator {
+
+    Stack<TreeNode> stack = new Stack();
+
+    public BSTIterator(TreeNode root) {
+        // partialorder
+        partialInOrder(root);
+
+    }
+
+    void partialInOrder(TreeNode node) {
+        while (node != null) {
+            stack.push(node);
+            node = node.left;
+        }
+    }
+
+    public int next() {
+        TreeNode top = stack.pop();
+        partialInOrder(top.right); // when ever you see a right, push the right into the stack
+        return top.val;
+
+    }
+
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+
+}
---
+++ DeleteNode.java
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/delete-node-in-a-linked-list/
+/*
+Time: O(1)
+Space: O(1)
+Did this code successfully run on Leetcode : Yes
+Any problem you faced while coding this : None
+*/
+public class DeleteNode {
+    public void deleteNode(ListNode node) {
+
+        node.val = node.next.val;
+
+        node.next = node.next.next;
+
+    }
+
+}
