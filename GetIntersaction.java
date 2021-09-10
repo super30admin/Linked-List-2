ead4d64243c389bf3f38392c5a3b12d262ca71b9
sandhya <goswami.sa@northeastern.edu>
2021-09-09 23:39:49 -0400

    completed GetIntersaction and ReorderList

---
+++ GetIntersaction.java
@@ -0,0 +1,82 @@
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+
+1. find length of both the linked list
+2. set count = biglength - small length
+3. bring the start pointers of both the list at same index 
+4. now start moving in both the linked list until listAponiet == listBpointer
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        
+        int countA = 0;
+         ListNode dummyA =   headA;
+          ListNode dummyB =   headB;
+        
+        if(headA == headB)
+        {
+            return headA;
+        }
+        while(dummyA != null)
+        {
+            countA++;
+            dummyA = dummyA.next;
+        }
+        
+        int countB = 0;
+        while(dummyB != null)
+        {
+            countB++;
+            dummyB = dummyB.next;
+        }
+        int headAindex = 0;
+        if(countA > countB)
+        {
+            int count = countA - countB;
+                while(count>0)
+                {
+                    count--;
+                    headA=headA.next;
+                }
+        }
+        else if(countB > countA)
+        {
+            
+            int count = countB - countA;
+                while(count >0)
+                {
+                    count--;
+                    headB=headB.next;
+                }
+        }
+        
+        while(headA != null  || headB != null)
+        {
+            if(headA == headB)
+            {
+                return headA;
+            }
+             headB = headB.next;
+             headA = headA.next;
+        }
+        
+        
+         return null;   
+    }
+}
---
+++ ReOrderList.java
@@ -0,0 +1,77 @@
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : no
+// Any problem you faced while coding this : getting error :Error - Found cycle in the ListNode
+
+
+// Your code here along with comments explaining your approach
+
+1. find middle
+2. reverse the second half 
+3. merge both the list (fist half and second half 
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        // null case
+        if(head == null)
+            return ;
+        
+       // find middle
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        ListNode middle = slow;
+        
+        // reverse second list (second half after middle point)
+        
+       ListNode secondList = reverseList(middle);
+        
+        
+        
+        // merge two sorted lists
+        ListNode first = head;
+      //  ListNode secondList = prev;
+        while(secondList != null)
+        {
+            ListNode temp =  first.next;
+            first.next = secondList;
+            first = temp;
+            
+            temp = secondList.next;
+            secondList.next = first;
+            secondList = temp;
+            //head = first;
+          
+            
+        }
+        
+        
+    }
+    
+    private ListNode reverseList(ListNode start)
+    {
+        if(start == null || start.next == null)
+            return start;
+         ListNode ele =  reverseList(start.next);
+        start.next.next = start;
+        start.next = null;
+        
+        return ele;
+    }
+}
