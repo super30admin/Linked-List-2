92288f169c1287b57edba5ff8db85416aad32c96
Sai Sruthi Mareedu <smareed1@sundevils.asu.edu>
2021-10-05 07:54:32 -0700

    completed

--- Sample.java
+++ Sample.java
@@ -1,7 +1,19 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : o (n)
+// Space Complexity : o(n)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
 
 
 // Your code here along with comments explaining your approach
+
+
+class Solution
+{
+   public void deleteNode(Node del)
+   {
+    Node temp=del;
+    temp=temp.next;
+    del=temp;
+   }
+
+}
---
+++ intersect.java
@@ -0,0 +1,42 @@
+// Time Complexity : o(n)
+// Space Complexity : o(1)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
+
+
+// Your code here along with comments explaining your approach
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+    Set<ListNode> nodesInB = new HashSet<ListNode>();
+
+        while (headB != null) {
+            nodesInB.add(headB);
+            headB = headB.next;
+        }
+
+        while (headA != null) {
+            // if we find the node pointed to by headA,
+            // in our set containing nodes of B, then return the node
+            if (nodesInB.contains(headA)) {
+                return headA;
+            }
+            headA = headA.next;
+        }
+
+        return null;
+    }
+            
+    }
