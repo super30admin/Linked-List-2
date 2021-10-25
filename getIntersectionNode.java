de6becdbeffbea5194215bdec4cf0293eede5b9f
panth1921 <panthdev96@gmail.com>
2021-10-25 00:38:35 -0400

    Linked-List-2

---
+++ getIntersectionNode.java
@@ -0,0 +1,43 @@
+// Time Complexity : 
+// Space Complexity :
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+
+    func getIntersectionNode(_ headA: ListNode?, _ headB: ListNode?) -> ListNode? {
+        var nodeA = headA
+        var nodeB = headB
+        var lengthA = 0
+        var lengthB = 0
+        while nodeA != nil {
+            nodeA = nodeA?.next
+            lengthA += 1
+        }
+        while nodeB != nil {
+            nodeB = nodeB?.next
+            lengthB += 1
+        }
+        nodeA = headA
+        nodeB = headB
+        var offset = 0
+        if lengthA > lengthB {
+            offset = lengthA - lengthB
+            while offset > 0 {
+                nodeA = nodeA?.next
+                offset -= 1
+            }
+        } else {
+            offset = lengthB - lengthA
+            while offset > 0 {
+                nodeB = nodeB?.next
+                offset -= 1
+            }
+        }
+        while nodeA != nil && nodeA !== nodeB {
+            nodeA = nodeA?.next
+            nodeB = nodeB?.next
+        }
+        return nodeA
+    }
---
+++ reorderList.java
@@ -0,0 +1,44 @@
+// Time Complexity : 
+// Space Complexity :
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+
+    func reorderList(_ head: ListNode?) {
+        var slow = head
+        var fast = head?.next?.next
+        while fast != nil {
+            fast = fast?.next?.next
+            slow = slow?.next
+        }
+       
+        var nodeb1 = reverse(slow?.next)
+        
+        slow?.next = nil
+        //print(slow?.val)
+        var nodea1 = head
+        
+        while  nodea1 != nil && nodeb1 != nil {
+            var nodea2 = nodea1?.next
+            var nodeb2 = nodeb1?.next
+            nodea1?.next = nodeb1
+            nodeb1?.next = nodea2
+            nodea1 = nodea2
+            nodeb1 = nodeb2
+            
+        }  
+    }
+    func reverse(_ head: ListNode?) -> ListNode? {
+        var node1 : ListNode?
+        var node2 = head
+        var node3 = head?.next
+        while node2 != nil {
+            node2?.next = node1
+            node1 = node2
+            node2 = node3
+            node3 = node3?.next
+        }
+        return node1
+    }
