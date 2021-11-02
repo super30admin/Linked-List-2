09b5cf57ed69188459e754460930da52019a6139
Adhish Shrivastava <adhish70@gmail.com>
2021-11-02 09:03:27 -0400

    Problem52 done

--- Problem-52 BST Iterator.py
+++ Problem-52 BST Iterator.py
@@ -1,10 +1,12 @@
 # 173. Binary Search Tree Iterator
 #  https://leetcode.com/problems/binary-search-tree-iterator/
 
-# Time Complexity: 
-# Space Complexiety: 
+# Time Complexity: O(1)
+# Space Complexiety: O(1)
 
-# Logic: 
+# Logic: We will have a global stack that at any instant will have the entire left branch. 
+# When next() is called we pop the stack and fill in the right branch. 
+# hasNext() will check the length of the global stack.
 
 # Definition for a binary tree node.
 # class TreeNode:
@@ -15,14 +17,26 @@
 class BSTIterator:
 
     def __init__(self, root: Optional[TreeNode]):
-        pass
-
+        self.temp_stack = list()
+        self._helper(root)
+        
+        
+    def _helper(self,root):
+        if root:
+            self.temp_stack.append(root)
+            self._helper(root.left)
+    
+    
     def next(self) -> int:
-        pass        
+        nextval = self.temp_stack.pop()
+        self._helper(nextval.right)
+        
+        return nextval.val
 
     def hasNext(self) -> bool:
-        pass
-
+        if len(self.temp_stack) != 0:
+            return True
+        return False
 
 # Your BSTIterator object will be instantiated and called as such:
 # obj = BSTIterator(root)
