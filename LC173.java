5a32bedd29944fb02bcfa70e2c3b48a6c53cf126
NachiketBavadekar <nachiket.bavadekar@gmail.com>
2022-04-03 12:50:50 -0500

    Competitive-Coding-3 in Java

---
+++ LC173.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BSTIterator {
+
+    Stack<TreeNode> stack;
+    
+    public BSTIterator(TreeNode root) {
+        
+        stack = new Stack<>();
+        dfs(root);
+    }
+    
+    private void dfs(TreeNode root)
+    {
+        while(root!=null)
+        {
+            stack.push(root);
+            root = root.left;
+        }
+    }
+    
+    public int next() {
+        
+        TreeNode result = stack.pop();
+        dfs(result.right);
+        return result.val; 
+        
+    }
+    
+    public boolean hasNext() {
+        
+        return !stack.isEmpty();
+    }
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
