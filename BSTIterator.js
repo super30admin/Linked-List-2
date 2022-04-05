74d0e5b68a77188de9dc21790bacc05a6c228849
Krina Patel <kp42942n@pace.edu>
2022-04-04 23:04:59 -0400

    adding where n is

--- BSTIterator.js
+++ BSTIterator.js
@@ -1,4 +1,4 @@
-// Time Complexity : O(n)
+// Time Complexity : O(n) where n is number of tree nodes
 // Space Complexity : O(n)
 // Did this code successfully run on GeeksForGeeks : Yes
 // Any problem you faced while coding this : No
