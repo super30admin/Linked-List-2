47b4ffebee1feaa2b2c66632b08dedf1d6083cfa
tanmay24497 <tanmayagrawal244@gmail.com>
2022-04-20 13:05:15 -0700

    LinkedList-2 Completed

---
+++ Reorder List.java
@@ -0,0 +1,56 @@
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+// first we find the middle elemnt of the linkedlist and then we will reverse the second half of the linkedlist. After we will split the linkedlist and put the pointer at the head of each half and the merge both the linkedlist.
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        if(head ==null||head.next==null)
+            return;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast.next!=null&&fast.next.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        fast = reverse(slow.next);
+        slow.next = null;
+        slow = head;
+        while(fast!=null){
+            ListNode temp = slow.next;
+            slow.next = fast;
+            fast = fast.next;
+            slow.next.next = temp;
+            slow = temp;
+        }
+    }
+    private ListNode reverse(ListNode head){
+        if(head==null||head.next==null)
+            return head;
+        ListNode curr = head;
+        ListNode prev = null;
+        ListNode next = head.next;
+        while(next!=null){
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+            next = next.next;
+        }
+        curr.next = prev;
+        return curr;
+    }
+}
