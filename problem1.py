99f93703ed017b2a48175b433de69556f86a78f5
Sanskar Gyawali <sanskargyawali@Sanskars-MacBook-Pro.local>
2022-05-02 22:21:56 -0700

    Finished problem1

--- problem1.py
+++ problem1.py
@@ -1,2 +1,30 @@
 #binary search tree iterator
-#havent gone over this one yet
+# // Time Complexity : O(1) asymptotically
+# // Space Complexity : O(H) - height of the tree
+# // Did this code successfully run on Leetcode :yes
+# // Any problem you faced while coding this :no
+
+class BSTIterator(object):
+
+    def __init__(self, root):
+        self.stack=[]
+        self._dfs(root)
+        
+        """
+        :type root: TreeNode
+        """
+    def _dfs(self, root):                                   #copy all the left nodes of the tree
+        while(root):
+            self.stack.append(root)                         
+            root=root.left
+        
+        
+
+    def next(self):
+        root = self.stack.pop()                                         #pop the currernt top of the stack
+        self._dfs(root.right)                                           #check if there is a right, if ues, add that then add all the left after that
+        return root.val
+        
+
+    def hasNext(self):
+        return self.stack
