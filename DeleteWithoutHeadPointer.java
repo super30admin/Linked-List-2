ffaeaf682071584809f106d087fcb07e166da0b2
jayachandravvs <42566670+jayachandravvs@users.noreply.github.com>
2022-05-19 19:34:11 -0500

    Update DeleteWithoutHeadPointer.java

--- DeleteWithoutHeadPointer.java
+++ DeleteWithoutHeadPointer.java
@@ -1,1 +1,14 @@
 
+// O(1) time || o(1) space
+class Solution
+{
+    public:
+    //Function to delete a node without any reference to head pointer.
+    void deleteNode(Node *del)
+    {
+       // Your code here
+      del.data= del.next.data
+       del.next=del.next.next;
+    }
+
+};
