27d0d6e9c41f7ea1d8ce25229798205022ff77ff
arshmohd <irfan.arshad4@gmail.com>
2022-05-20 23:58:00 +0530

    Implemented the algos for BST Iterator, delete node, intersection Lists and Reorder list

---
+++ IntersectionList.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) {
+ * val = x;
+ * next = null;
+ * }
+ * }
+ */
+public class IntersectionList {
+  // Time O(N)
+  // Space O(1)
+
+  public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+      val = x;
+      next = null;
+    }
+  }
+
+  public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+    ListNode curr = headA;
+    int len_A = 0, len_B = 0;
+    while (curr != null) {
+      curr = curr.next;
+      len_A++;
+    }
+    curr = headB;
+    while (curr != null) {
+      curr = curr.next;
+      len_B++;
+    }
+    ListNode currA = headA, currB = headB;
+    while (len_A > len_B) {
+      currA = currA.next;
+      len_A--;
+    }
+    while (len_B > len_A) {
+      currB = currB.next;
+      len_B--;
+    }
+    while (currA != currB) {
+      currA = currA.next;
+      currB = currB.next;
+    }
+    return currA;
+  }
+}
