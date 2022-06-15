df2be32834984bd1edea6d08b5646b74cdf0f424
Vraj Himanshu Reshamdalal <30725458+vraj9845@users.noreply.github.com>
2022-06-15 10:24:03 -0400

    Create Solution.java

---
+++ intersection of two linked list/Solution.java
@@ -0,0 +1,16 @@
+// Time Complexity:O(a+b)
+// Space Complexity: O(1)
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA==null || headB==null){
+            return null;
+        }
+        ListNode a = headA;
+        ListNode b = headB;
+        while(a!=b){
+            a= a==null ? headB:a.next;
+            b= b==null ? headA:b.next;
+        }
+        return a;
+    }
+}
