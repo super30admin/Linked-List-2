48958396d18f86b6fec04a2db1d66c68b74bfa5d
Sahithi Kamireddy <sahithi.kamireddy@gmail.com>
2022-06-20 22:37:29 -0700

    Completed BSTIterator

---
+++ BinarySearchTreeIterator.java
@@ -0,0 +1,52 @@
+/* Time Complexity: O(1) - next(), hasNext()
+ * Space Complexity: O(n)
+ * Did this code successfully run on Leetcode : yes
+ * Any problem you faced while coding this : No
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BSTIterator {
+    Stack<TreeNode> st;
+    public BSTIterator(TreeNode root) {
+        st = new Stack<>();
+        dfs(root);
+    }
+    private void dfs(TreeNode root) {
+        while(root != null){
+            st.push(root);
+            root = root.left;
+        }
+    }
+    
+    public int next() {
+        TreeNode curr = st.pop();
+        int temp = curr.val;
+        dfs(curr.right);
+        return temp;
+    }
+    
+    public boolean hasNext() {
+        return !st.isEmpty();
+    }
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
