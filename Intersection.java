114ab0b632abd102fff6438f7710104ab29ee9d1
SwatiKedarAgarwal <sxa190153@utdallas.edu>
2022-06-24 19:13:16 -0500

    Linked List 2 exercises

---
+++ Intersection.java
@@ -0,0 +1,31 @@
+// Time Complexity : O(m+n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Intersect {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        
+        ListNode pointerA = headA;
+        ListNode pointerB = headB;
+        while (pointerA != pointerB) {
+            pointerA = pointerA == null ? headB : pointerA.next;
+            pointerB = pointerB == null ? headA : pointerB.next;
+        }
+        return pointerA;
+    }
+}
