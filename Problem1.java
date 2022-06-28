0319593455c48ad8ca18f3abddd2206310282eaa
ShwetaJose00 <91407352+shwetajose@users.noreply.github.com>
2022-06-28 15:33:19 -0400

    Completed

---
+++ Problem1.java
@@ -0,0 +1,50 @@
+// Time Complexity : O(n) 
+// Space Complexity : O(logn)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+//173. Binary Search Tree Iterator
+//https://leetcode.com/problems/binary-search-tree-iterator/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Problem1 {
+
+    Stack<TreeNode> st;
+
+    public BSTIterator(TreeNode root) {
+        st = new Stack<>();
+        dfs(root);
+    }
+
+    private void dfs(TreeNode root) {
+        while (root != null) {
+            st.push(root);
+            root = root.left;
+        }
+    }
+
+    public int next() {
+        TreeNode curr = st.pop();
+        int temp = curr.val;
+        dfs(curr.right);
+        return temp;
+    }
+
+    public boolean hasNext() {
+        return !st.isEmpty();
+    }
+}
