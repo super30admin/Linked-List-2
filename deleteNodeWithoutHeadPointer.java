1d7d3764cbdd2e08ebf7d71f6ebcb984f71d28a5
STSC <STSC@LENOVO-127795.ad.csueastbay.edu>
2022-07-05 13:31:01 -0700

    Complete

---
+++ deleteNodeWithoutHeadPointer.java
@@ -0,0 +1,13 @@
+// Time Complexity: O(1)
+// Space Complexity: O(1)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : No
+
+class Solution
+{
+    void deleteNode(Node del)
+    {
+         del.data=del.next.data;
+         del.next=del.next.next;
+    }
+}
