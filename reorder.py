43ec1f4d33c3747937f6f6702912cb0f4aaded7b
harjotsbhatia <89289963+harjotsbhatia@users.noreply.github.com>
2022-08-02 19:18:03 -0700

    Create reorder.py

---
+++ reorder.py
@@ -0,0 +1,23 @@
+# Tc: O(n)
+# SC: O(1)
+class Solution:
+    def reorderList(self, head: ListNode) -> None:
+        if not head:
+            return 
+        
+       
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next 
+            
+       
+        prev, curr = None, slow
+        while curr:
+            curr.next, prev, curr = prev, curr, curr.next       
+
+        
+        first, second = head, prev
+        while second.next:
+            first.next, first = second, first.next
+            second.next, second = first, second.next
