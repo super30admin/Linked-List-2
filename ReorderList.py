84ac87801866132bbb8766c554270f8e7e8aead9
Raj Shah <rajshah1998.d@gmail.com>
2022-08-09 21:35:25 -0400

    Completed Linked List - 2

---
+++ ReorderList.py
@@ -0,0 +1,51 @@
+# Time Complexity : O(N) where N is number of nodes in the linked list
+# Space Complexity : O(1)
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        count = 1
+        slow = head
+        fast = head
+
+        while fast.next is not None and fast.next.next is not None:
+            count += 1
+            slow = slow.next
+            fast = fast.next.next
+
+        if count <= 1:
+            return
+
+        end = self.reverse(slow.next)
+        slow.next = None
+        start = head
+        while end is not None:
+            t = start.next
+            start.next = end
+            end = end.next
+            start.next.next = t
+            start = start.next.next
+
+
+    def reverse(self, node):
+        prev = None
+        curr = node
+        lead = node.next
+
+        while curr.next is not None:
+            curr.next = prev
+            prev = curr
+            curr = lead
+            lead = lead.next
+
+        curr.next = prev
+
+        return curr
