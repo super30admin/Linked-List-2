002f9a2e3164f52f0d9d73febb0df196cf6e8e71
Jay Tanna <31876791+jaytanna61@users.noreply.github.com>
2022-08-16 18:38:13 -0400

    Update DeleteWithoutHead.java

--- DeleteWithoutHead.java
+++ DeleteWithoutHead.java
@@ -1,4 +1,7 @@
-
+// Time Complexity : O(1)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
 
 //Function to delete a node without any reference to head pointer.
 class Solution
