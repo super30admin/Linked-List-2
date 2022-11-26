c5995f7553e0198e7e753456cb03d7fba8ce6af5
Khushboo Gandhi <khushboo1028@gmail.com>
2022-11-26 15:19:03 -0800

    Linked List 2 Done

---
+++ bst-iterator.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+
+// TC is O(h)
+// MC is O(h)
+import java.util.*;
+
+class BSTIterator {
+    Stack<TreeNode> s;
+
+    public BSTIterator(TreeNode root) {
+        s = new Stack<>();
+        dfs(root);
+
+    }
+
+    public int next() {
+        TreeNode popped = s.pop();
+        dfs(popped.right);
+        return popped.val;
+
+    }
+
+    public boolean hasNext() {
+        return !s.isEmpty();
+    }
+
+    private void dfs(TreeNode root) {
+        while (root != null) {
+            s.push(root);
+            root = root.left;
+        }
+    }
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
