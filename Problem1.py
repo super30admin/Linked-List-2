7834b5a36ff99f2c9f8068cc00df425da64ba701
Rishab Parekh <rishabparekh14@gmail.com>
2022-11-28 10:17:42 -0800

    Sprint Linked Lisrt 2 completed

--- Problem1.py
+++ Problem1.py
@@ -39,3 +39,47 @@ class BSTIterator:
 # obj = BSTIterator(root)
 # param_1 = obj.next()
 # param_2 = obj.hasNext()
+
+
+
+
+### Another optimised approach
+
+#Time Complexity :- O(n)
+#Space Complexity:- O(h) height of the tree on which side we are traversiing
+#Appraoch :- Here we use stack to restrict the reursive call and when needed then only we add the next element in the tree when it has right side 
+
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class BSTIterator:
+
+    def __init__(self, root: Optional[TreeNode]):
+        self.stack = []
+        self.leftSideTree(root)
+    
+    def leftSideTree(self, root):
+        while root is not None:
+            self.stack.append(root)
+            root = root.left
+
+    def next(self) -> int:
+        popped = self.stack.pop()
+        self.leftSideTree(popped.right)
+        return popped.val
+        
+
+    def hasNext(self) -> bool:
+        return len(self.stack) > 0
+        
+
+
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
