8dfc085ea9a4ac6eb49f8c47246a0baa0ba25807
tirthaj.rikame@gmail.com <tirthaj.rikame@gmail.com>
2023-04-01 19:34:42 -0400

    Completed LL-2. Please review.

--- Delete without head pointer.py
+++ Delete without head pointer.py
@@ -14,7 +14,8 @@
 		        self.next = None
 	}
 '''
-
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 class Solution:
     # Function to delete a node without any reference to head pointer.
     def deleteNode(self, curr_node):
