e239e26d334e7218c43438ab72be600f01d339d8
DEEPIKA <170031561@kluniversity.in>
2023-07-31 20:30:47 -0400

    completed Linked-List-2 - please review

---
+++ deletenodeRef.py
@@ -0,0 +1,7 @@
+#TC - O(1)
+#SC - O(1)
+class Solution:
+    #Function to delete a node without any reference to head pointer.
+    def deleteNode(self,curr_node):
+        curr_node.data = curr_node.next.data
+        curr_node.next = curr_node.next.next
