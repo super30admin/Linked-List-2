5a24b648d6f7d76a3064cb215217fbec23207f14
rukhsard42 <rukhsard42@gmail.com>
2023-10-09 17:20:58 -0700

    Completed Linked List - 2
    Completed problem 1 and 3

--- Problem1.java
+++ Problem1.java
@@ -1,2 +1,33 @@
+// Time Complexity : O(1) s.pop() operation takes O(1) time
+// Space Complexity : O(h) where h is the height of tree and stack will store all left nodes of tree = height of the tree
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
 public class Problem1 {
+    Stack<TreeNode> s;
+
+    public BSTIterator(TreeNode root) {
+        s = new Stack<>();
+        dfs(root);
+    }
+
+    public int next() {
+        TreeNode next = s.pop();
+        dfs(next.right);
+        return next.val;
+    }
+
+    public boolean hasNext() {
+        if(s.isEmpty()){
+            return false;
+        }
+        return true;
+    }
+
+    private void dfs(TreeNode root){
+        while(root != null){
+            s.push(root);
+            root = root.left;
+        }
+    }
 }
--- Problem3.java
+++ Problem3.java
@@ -1,2 +1,14 @@
+// Time Complexity : O(1) we only change pointer to the node
+// Space Complexity : O(1) not using any extra space
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
 public class Problem3 {
+    void deleteNode(Node del)
+    {
+        // Your code here
+        Node next = del.next;
+        del.data = next.data;
+        del.next = next.next;
+    }
 }
