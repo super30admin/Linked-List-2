03bf9c1b9e0d8508cea8ae60ffae65d6a4425cfc
Jaissica <jaissica.hora@gmail.com>
2023-10-18 14:15:22 -0400

    Create Intersection of Two Linked Lists-160.java

---
+++ Intersection of Two Linked Lists-160.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+
+        ListNode curr = headA;
+        int lenA = 0;
+        while (curr != null) {
+            lenA++;
+            curr = curr.next;
+        }
+        curr = headB;
+        int lenB = 0;
+        while (curr != null) {
+            lenB++;
+            curr = curr.next;
+        }
+
+        while (lenA > lenB) {
+            headA = headA.next;
+            lenA--;
+        }
+
+        while (lenB > lenA) {
+            headB = headB.next;
+            lenB--;
+        }
+
+        while (headA != headB) {
+            headA = headA.next;
+            headB = headB.next;
+        }
+        return headA;        
+    }
+}
