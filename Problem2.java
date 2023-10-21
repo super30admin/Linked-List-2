1d33294aed3df2165dadcc749d04aa8260d4d260
Aditya Kondepudi <36687530+adityakondepudi@users.noreply.github.com>
2023-10-20 22:13:35 -0400

    Create Problem2.java

---
+++ Problem2.java
@@ -0,0 +1,43 @@
+//Time Complexity : O(n)
+//Space complexity : O(1)
+
+
+class Solution {
+    public void reorderList(ListNode head) {
+        if(head == null || head.next == null || head.next.next == null) return;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast.next!=null && fast.next.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        fast = reverse(slow.next);
+        slow.next = null;
+
+        slow = head;
+        while (fast!=null){
+            ListNode temp = slow.next;
+            slow.next = fast;
+            fast = fast.next;
+            slow.next.next = temp;
+            slow=temp;
+        }
+    }
+    private ListNode reverse(ListNode head){
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode fast = head.next;
+
+        while(fast !=null ){
+            curr.next = prev;
+            prev = curr;
+            curr = fast;
+            fast = fast.next;    
+        }
+        curr.next = prev;
+        return curr;
+    }
+}
